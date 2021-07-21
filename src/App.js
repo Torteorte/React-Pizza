9fee980ee26bc28ae4718236edc26ebfaa789cf9
Torteorte <lipstort@gmail.com>
2021-07-21 21:46:28 +0300

    add axios, add json local server. refactoring pizza-block

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import Header from './Components/Header/Header';
-import MainPage from './Components/Content/MainPage';
+import MainPage from './Components/MainPage/MainPage';
 import Cart from './Components/Cart/Cart';
 import CartEmpty from './Components/CartEmpty/CartEmpty';
 import { Redirect, Route } from 'react-router-dom';
+import React from 'react';
+import axios from "axios";
 
 const App = () => {
+
+  const [pizzas, setPizzas] = React.useState([])
+
+  React.useEffect(() => {
+    axios('http://localhost:3000/db.json')
+      .then((response) => {
+        return response.pizzas
+      })
+  }, [])
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((resp) => resp.json())
+      .then(json => {
+        setPizzas(json.pizzas)
+      })
+  }, [])
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Route exact path='/' render={() => <Redirect from="/" to="/mainpage" />} />
-        <Route path='/mainpage' render={() => <MainPage />} />
+        <Route path='/mainpage' render={() => <MainPage pizzas={pizzas} />} />
         <Route path='/Cart' render={() => <Cart />} />
         <Route path='/CartEmpty' render={() => <CartEmpty />} />
         <Route path='*' render={() => <Redirect to="/mainpage" />} />
--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,20 +1,18 @@
 import Navbar from "./Navbar/Navbar"
 import PizzaItem from "./PizzaItem/PizzaItem"
 
-const MainPage = () => {
+const MainPage = (props) => {
    return (
       <div className="container">
          <Navbar />
          <h2 className="content__title">Все пиццы</h2>
          <div className="content__items">
-            <PizzaItem />
-            <PizzaItem />
-            <PizzaItem />
-            <PizzaItem />
-            <PizzaItem />
-            <PizzaItem />
-            <PizzaItem />
-            <PizzaItem />
+            {props.pizzas.map((item, index) => {
+               return <PizzaItem
+                  key={item.id}
+                  item={item}
+               />
+            })}
          </div>
       </div>
    )
